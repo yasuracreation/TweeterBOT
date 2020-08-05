3cb323d02a44daf222663c6a44f6c9ac82989e38
Yasura <yasuracreation@gmail.com>
2020-08-05 18:00:45 +0530

    tweeter bot changes

--- tweet.py
+++ tweet.py
@@ -10,26 +10,27 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 user = api.me()
-print (user.name) #prints your name.
-print (user.screen_name)
-print (user.followers_count)
+print(user.name)  # prints your name.
+print(user.screen_name)
+print(user.followers_count)
 
 search = "zerotomastery"
 numberOfTweets = 2
 
+
 def limit_handle(cursor):
-  while True:
-    try:
-      yield cursor.next()
-    except tweepy.RateLimitError:
-      time.sleep(1000)
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            time.sleep(1000)
 
-#Be nice to your followers. Follow everyone!
+
+# Be nice to your followers. Follow everyone!
 for follower in limit_handle(tweepy.Cursor(api.followers).items()):
-  if follower.name == 'Usernamehere':
-    print(follower.name)
-    follower.follow()
-
+    if follower.name == 'Usernamehere':
+        print(follower.name)
+        follower.follow()
 
 # Be a narcisist and love your own tweets. or retweet anything with a keyword!
 for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
